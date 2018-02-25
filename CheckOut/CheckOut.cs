fdeb1eb3e2d0376fe6c28d57c6a3eae2efb95bb9
Ali IYBAR <ali.iybar@gmail.com>
2018-02-25 20:43:54 +0000

    Test Passed

--- CheckOut/CheckOut.cs
+++ CheckOut/CheckOut.cs
@@ -1,13 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using CheckOut.Models;
 
 namespace CheckOut
 {
     public class CheckOut : ICheckOut
     {
+        private readonly List<string> _basket = new List<string>();
+        private readonly List<Item> _items;
+        public int TotalItemCount = 0;
+        
+        public CheckOut(List<Item> items)
+        {
+            _items = items;
+        }
+       
         public void Scan(string item)
         {
-            throw new NotImplementedException();
+            if (_items.Exists(k => k.Sku == item))
+            {
+              _basket.Add(item);
+              TotalItemCount += 1;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            
         }
+         
 
         public int GetTotalPrice()
         {
