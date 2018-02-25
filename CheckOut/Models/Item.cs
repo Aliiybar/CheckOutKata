e905b37026db9b219a7c43ba3d3e2b19e5daa564
Ali IYBAR <ali.iybar@gmail.com>
2018-02-25 20:04:36 +0000

    Item model is added

---
+++ CheckOut/Models/Item.cs
@@ -0,0 +1,18 @@
+﻿namespace CheckOut.Models
+{
+    public class Item
+    {
+
+        public Item(string sku, int unitPrice, int discountQuantity, int discountedTotal)
+        {
+            Sku = sku;
+            UnitPrice = unitPrice;
+            DiscountQuantity = discountQuantity;
+            DiscountedTotal = discountedTotal;
+        }
+        public string Sku { get; private set; }
+        public int UnitPrice { get; private set; }
+        public int DiscountQuantity { get; private set; }
+        public int DiscountedTotal { get; private set; }
+    }
+}
