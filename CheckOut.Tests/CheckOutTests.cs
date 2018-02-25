7609622a9d340db055a35ffea20832061f14ae36
Ali IYBAR <ali.iybar@gmail.com>
2018-02-25 20:18:07 +0000

    Test item list is created with given discount terms

--- CheckOut.Tests/CheckOutTests.cs
+++ CheckOut.Tests/CheckOutTests.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using CheckOut.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CheckOut.Tests
@@ -5,6 +7,20 @@ namespace CheckOut.Tests
     [TestClass]
     public class CheckOutTests
     {
+        private List<Item> _items;
+        
+        [TestInitialize]
+        public void SetUp()
+        {
+            _items = new List<Item>
+            {
+                new Item("A", 50, 3, 130),
+                new Item("B", 30, 2, 45),
+                new Item("C", 20, 0, 0),
+                new Item("D", 15, 0, 0)
+            };
+        }
+        
         [TestMethod]
         public void TestMethod1()
         {
