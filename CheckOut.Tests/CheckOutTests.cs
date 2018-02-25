1e4c4367c07f279e01c15a64e1ef050c77653afd
Ali IYBAR <ali.iybar@gmail.com>
2018-02-25 19:54:09 +0000

    Class Project And MSTest Project created

---
+++ CheckOut.Tests/CheckOutTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CheckOut.Tests
+{
+    [TestClass]
+    public class CheckOutTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
